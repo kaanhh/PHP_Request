b9fe43849a676c41a9b93b9797823c94ae4f566b
Kaan Ertugrul <mail@mail.com>
2022-08-24 22:14:11 +0200

    probearbeit_version_0.1

---
+++ registration.php
@@ -0,0 +1,38 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WTC Registrierung</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Registrierung</h1>
+        </div>
+
+        <form action="registration.php" method="post">
+            <div>
+                <label for="vorname">Vorname: </label>
+                <input type="text" name="vorname" required>
+            </div>
+            <div>
+                <label for="nachname">Nachname: </label>
+                <input type="text" name="nachname" required>
+            </div>
+            <div>
+                <label for="email">E-Mail: </label>
+                <input type="text" name="email" required>
+            </div>
+            <div>
+                <label for="firma">Firma: </label>
+                <input type="text" name="firma" required>
+            </div>
+            <button type="submit" name="reg_benutzer">Absenden</button>
+        </form>
+    
+    </div>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,69 @@
+<?php  
+/***********************************
+Autor: KAAN der Unermüdliche
+Verion: 0.1
+INFO: Bewerbungsaufgabe, Registrierung von Usern auf Website und speichern in Datenbank in PHP
+
+*************************************/
+
+session_start();
+
+////////////////////////////////////////////////////////////////////
+//Variablen initialisieren
+
+$vorname = "";
+$nachname = "";
+$email = "";
+$firma = "";
+
+$errors = array();
+
+////////////////////////////////////////////////////////////////////
+//Mit der Datenbank verbinden oder Fehler ausgeben
+$db = mysqli_connect('mysql04.manitu.net','u82468','6TnMwXcU6wxAQ13U', 'db82468') or die("Konnte nicht mit Datenbank verbinden");
+
+
+////////////////////////////////////////////////////////////////////
+//Benutzer registrieren
+$vorname = mysqli_real_escape_string($db, $_POST['vorname']);
+$nachname = mysqli_real_escape_string($db, $_POST['nachname']);
+$email = mysqli_real_escape_string($db, $_POST['email']);
+$firma = mysqli_real_escape_string($db, $_POST['firma']);
+
+
+////////////////////////////////////////////////////////////////////
+//Form Validierung, Fehler ausgeben wenn Felder leer
+if(empty($vorname)) {
+    array_push($errors, "Vorname darf nicht leer sein!");
+}
+if(empty($nachname)) {
+    array_push($errors, "Nachname darf nicht leer sein!");
+}
+if(empty($email)) {
+    array_push($errors, "Email darf nicht leer sein!");
+}
+if(empty($firma)) {
+    array_push($errors, "Firma darf nicht leer sein!");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//Überprüfe, ob gleiche Person (email) bereits in Datenbank existiert und gib Fehler aus
+$benutzer_check_query = "SELECT * FROM benutzer WHERE wtc_email = '$email' LIMIT 1";
+
+$ergebnis = mysqli_query($db, $benutzer_check_query);
+$benutzer = mysqli_fetch_assoc($ergebnis);
+
+if($benutzer){
+    if($benutzer['email'] === $email) {
+        array_push($errors, "Ihre Email ist bereits registriert!");
+    }
+}
+
+////////////////////////////////////////////////////////////////////
+//Registrieren ohne Fehler und Eingabe in Datenbank abspeichern
+if(count($errors) == 0){
+    $query = "INSERT INTO benutzer(wtc_vorname, wtc_nachname, wtc_email , wtc_firma) VALUES ('$vorname','$nachname', '$email', '$firma')";
+
+    mysqli_query($db, $query);
+}
+?>
